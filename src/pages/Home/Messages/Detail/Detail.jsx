cc227ac5b1df39e9427e24bd2673932709e11cdd
Jiazhen Guo <Jen06373065@163.com>
2021-07-25 21:26:17 +0930

    params

--- src/pages/Home/Messages/Detail/Detail.jsx
+++ src/pages/Home/Messages/Detail/Detail.jsx
@@ -1,30 +1,28 @@
 import React, { Component } from 'react'
-const dataDetails=[
+const dataDetails = [
     {
-        id:'1',
-        title:'message 01',
-        content:'hello,dear!'
+        id: '1',
+        title: 'message 01',
+        content: 'hello,dear!'
     },
     {
-        id:'2',
-        title:'message 02',
-        content:'hello,my skills!'
+        id: '2',
+        title: 'message 02',
+        content: 'hello,my skills!'
     },
     {
-        id:'3',
-        title:'message 03',
-        content:'hello, your future!'
+        id: '3',
+        title: 'message 03',
+        content: 'hello, your future!'
     },
 ]
 export default class Detail extends Component {
-   
-   
     render() {
-         const {id}=this.props.match.params
-         const findResult=dataDetails.find((findObj)=>{
-             return findObj.id===id
+        const { id } = this.props.match.params
+        const findResult = dataDetails.find((findObj) => {
+            return findObj.id === id
 
-         })
+        })
         console.log(this.props)
         return (
             <ul>
