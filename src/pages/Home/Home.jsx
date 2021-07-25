22bfff25b4ba6610dd16a5d9de7920957137aa5f
Jiazhen Guo <Jen06373065@163.com>
2021-07-25 14:43:30 +0930

    secondary routing

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import { NavLink, Switch, Route } from 'react-router-dom';
+import News from './News';
+import Messages from './Messages';
+const Nav = styled.ul`
+display:flex;
+`;
+const NavLi=styled.li`
+list-style-type:none;
+width:100px;
+height:20px;
+padding:10px;
+border:1px solid #ddd;
+text-align:center`;
+
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <h2>Home</h2>
+                <h2>Home‘s NavLists ...</h2>
+                <br />
+                <Nav>
+                    <NavLi>
+                        <NavLink to="/home/news">NEWS</NavLink>
+                    </NavLi>
+                    <NavLi>
+                        <NavLink to='/home/messages'>MESSAGES</NavLink>
+                    </NavLi>
+                </Nav>
+                <Switch>
+                    <Route path='/home/news' component={News} />
+                    <Route path='/home/messages' component={Messages} />
+                </Switch>
+
             </div>
         )
     }
